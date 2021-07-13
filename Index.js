85ef3bda3dc53b2d2fbf55fe9b3d20190e9314b5
Jose Rosas <josejrrosas@yahoo.com>
2021-07-12 20:34:43 -0500

    comepleted lib files. started pprompt for employeeInput function.
    qe!

--- Index.js
+++ Index.js
@@ -1,5 +1,4 @@
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// When app start I am prompted to enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 // WHEN I select the engineer option
 // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
@@ -9,11 +8,23 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const promptUser = () => {
+const employeeInput = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
+            message: 'What is your name?',
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your name?',
+        },
+
+        {
+            type: 'input',
+            name: 'email',
             message: 'What is your name?',
         }
 
